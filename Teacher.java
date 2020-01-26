18163117cc38e997167061dade39486083124867
Patel-Arti-B <56549796+Patel-Arti-B@users.noreply.github.com>
2020-01-26 10:44:33 +0530

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,9 @@
+
+public class Teacher {
+	String designation="Teacher";
+	String collegeName="Arrdekta";
+	void does()
+	{
+		System.out.println("Teaching......");
+	}
+}
---
+++ TeacherTester.java
@@ -0,0 +1,10 @@
+
+public class TeacherTester {
+	public static void main(String[] args) {
+		PhysicsTeacher ph=new PhysicsTeacher();
+		System.out.println(ph.designation);
+		System.out.println(ph.collegeName);
+		System.out.println(ph.subject);
+		ph.does();
+	}
+}
